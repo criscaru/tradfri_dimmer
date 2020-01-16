51d93c65572e8f140e60032471feeb2d4168bc66
criscaru <43373844+criscaru@users.noreply.github.com>
2020-01-16 23:05:54 +0100

    Update remote_control.py

--- apps/remote_control/remote_control.py
+++ apps/remote_control/remote_control.py
@@ -11,24 +11,24 @@ class RemoteControl(hass.Hass):
             self.log(data["event"])
             if data["event"] == 1002:
                 self.log("Fast Right - light on")
-                self.call_service("light/turn_on", entity_id="light.comodinoci")
+                self.call_service("light/turn_on", entity_id=self.args["dimlight"])
             elif data["event"] == 2002:
-                if self.get_state("light.comodinoci") == "on":
-                    current_brightness = self.get_state('light.comodinoci', attribute='brightness')
-                    new_brightness = current_brightness + 25
+                if self.get_state(self.args["dimlight"]) == "on":
+                    current_brightness = self.get_state(self.args["dimlight"], attribute='brightness')
+                    new_brightness = current_brightness + self.args["step"]
                     if new_brightness > 254:
                         new_brightness=254
                     self.log("Slow Right - dim up (%i)",new_brightness )
-                    self.call_service('light/turn_on', entity_id='light.comodinoci', brightness=new_brightness, transition=2)
+                    self.call_service('light/turn_on', entity_id=self.args["dimlight"], brightness=new_brightness, transition=2)
             elif data["event"] == 3002:
                 
-                if self.get_state("light.comodinoci") == "on":
-                    current_brightness = self.get_state('light.comodinoci', attribute='brightness')
-                    new_brightness = current_brightness - 25
+                if self.get_state(self.args["dimlight"]) == "on":
+                    current_brightness = self.get_state(self.args["dimlight"], attribute='brightness')
+                    new_brightness = current_brightness - self.args["step"]
                     if new_brightness < 0:
                         new_brightness = 0
                     self.log("Slow Left - dim down (%i)", new_brightness)    
-                    self.call_service('light/turn_on', entity_id='light.comodinoci', brightness=new_brightness, transition=2)
+                    self.call_service('light/turn_on', entity_id=self.args["dimlight"], brightness=new_brightness, transition=2)
             elif data["event"] == 4002:
                 self.log("Fast Left - light off")
-                self.call_service("light/turn_off", entity_id="light.comodinoci"
+                self.call_service("light/turn_off", entity_id=self.args["dimlight"])
